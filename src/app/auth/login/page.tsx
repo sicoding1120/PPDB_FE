6a232a3cf6640ddb9a34d67affba59d8e398449f
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2025-05-19 14:53:00 +0700

    commit

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <section className='w-full h-screen flex'>
-      <div className='w-2/3 h-full bg-red-500'></div>
+    <section className='w-full h-screen flex '>
+          <div className='w-2/3 h-full bg-login bg-no-repeat bg-cover p-6'>
+          <div className=''></div>
+          </div>
       <div className='w-1/3 h-full px-4 py-4'>
-              <div className='w-full h-full bg-yellow-500 '>
-                  <h2></h2>
+        <div className='w-full h-full  '>
+          <h2></h2>
         </div>
       </div>
     </section>
