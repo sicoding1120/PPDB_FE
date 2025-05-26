2474976f39f3404fc0d9efd5316e4e40be238bdd
Argadida <egakarga369@gmail.com>
2025-05-26 10:52:58 +0700

    axios

--- lib/axiosClient.ts
+++ lib/axiosClient.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 export const axiosClient: AxiosInstance = axios.create({
-    
+  baseURL: "http://localhost:5000",
+  headers: { "Content-Type": "application/json" },
 });
+
+export default axiosClient;
